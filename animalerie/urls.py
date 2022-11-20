1af44bb482ddc7a228d932f6a1721d1a06fc4f40
adiouf <h8799r@gmail.com>
2022-11-20 18:10:30 +0100

    Problème d'url réglé et form opérationnel

--- animalerie/urls.py
+++ animalerie/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.animal_list, name='animal_list'),
+    path('animal/<str:id_animal>/', views.animal_detail, name='animal_detail'),
+    path('animal/<str:id_animal>/?<str:message>', views.animal_detail, name='animal_detail_mes'),
 ]
