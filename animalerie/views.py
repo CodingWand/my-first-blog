24d8bee239a447083d7e93f59b3e5a712d6207ef
adiouf <h8799r@gmail.com>
2022-11-20 14:57:41 +0100

    ajout de la vue d'accueil + création de template + ajout d'un peu de style

--- animalerie/views.py
+++ animalerie/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Animal
 
-# Create your views here.
+def animal_list(request):
+    animals = Animal.objects.all()
+    return render(request, 'animalerie/animal_list.html', {"animals" : animals})
